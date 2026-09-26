0b1f91590fb05aabb002424d989fc6078e1862d2
Saif Mansuri <Saifmansuri7860@gmail.com>
2026-09-26 21:48:20 +0530

    Register models in Django admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Movie, Theatre, Show, Seat, Booking, BookingSeat, OTP
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra Info', {'fields': ('phone_number', 'age', 'role', 'is_verified')}),
+    )
+    list_display = ('username', 'email', 'role', 'is_verified', 'is_staff')
+
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Movie)
+admin.site.register(Theatre)
+admin.site.register(Show)
+admin.site.register(Seat)
+admin.site.register(Booking)
+admin.site.register(BookingSeat)
+admin.site.register(OTP)
